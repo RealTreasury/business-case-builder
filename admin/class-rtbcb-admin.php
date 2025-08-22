d99ce47a49b658cd01e4cb2cd1b6854675384261
RealTreasury <tschultz@realtreasury.com>
2025-08-22 19:29:44 -0400

    Use dedicated preview nonce

--- admin/class-rtbcb-admin.php
+++ admin/class-rtbcb-admin.php
@@ -64,6 +64,7 @@ class RTBCB_Admin {
             'ajax_url'          => admin_url( 'admin-ajax.php' ),
             'nonce'             => wp_create_nonce( 'rtbcb_nonce' ),
             'diagnostics_nonce' => wp_create_nonce( 'rtbcb_diagnostics' ),
+            'report_preview_nonce' => wp_create_nonce( 'rtbcb_generate_report_preview' ),
             'strings'           => [
                 'confirm_delete'     => __( 'Are you sure you want to delete this lead?', 'rtbcb' ),
                 'confirm_bulk_delete'=> __( 'Are you sure you want to delete the selected leads?', 'rtbcb' ),
