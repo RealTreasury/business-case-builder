a394514a6cf24663911dcdf4ca7d2993485fb81a
RealTreasury <213981846+RealTreasury@users.noreply.github.com>
2025-08-29 15:15:44 -0400

    Add table creation to database initializer

--- inc/class-rtbcb-db.php
+++ inc/class-rtbcb-db.php
@@ -20,6 +20,8 @@ class RTBCB_DB {
      * @return void
      */
     public static function init() {
+        self::create_tables();
+
         $current = get_option( 'rtbcb_db_version', '1.0.0' );
 
         if ( version_compare( $current, self::DB_VERSION, '<' ) ) {
@@ -56,6 +58,31 @@ class RTBCB_DB {
 
         // Log the upgrade.
         error_log( 'RTBCB: Database upgraded from version ' . $from_version . ' to ' . self::DB_VERSION );
+    }
+
+    /**
+     * Create required database tables.
+     *
+     * @return void
+     */
+    private static function create_tables() {
+        global $wpdb;
+
+        $charset_collate = $wpdb->get_charset_collate();
+
+        // Create leads table.
+        $table_name = $wpdb->prefix . 'rtbcb_leads';
+        $sql        = "CREATE TABLE {$table_name} (
+            id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+            email varchar(255) NOT NULL,
+            company_size varchar(50),
+            industry varchar(100),
+            created_at datetime DEFAULT CURRENT_TIMESTAMP,
+            PRIMARY KEY (id)
+        ) {$charset_collate};";
+
+        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        dbDelta( $sql );
     }
 
     /**
